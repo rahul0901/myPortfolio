3f215d134a827078dae4a783f7f5346aba67f21b
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-12-10 20:45:46 +0530

    addd

---
+++ errahul/src/Components/Certificates.jsx
@@ -0,0 +1,11 @@
+import './Certificate.css';
+
+const Certificate = () => {
+    return(
+        <>
+            <div className='screen-certificate'>Certificate section</div>
+        </>
+    )
+}
+
+export default Certificate;
